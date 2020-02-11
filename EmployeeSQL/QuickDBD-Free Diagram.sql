6a4f01e594c64271b390cbf4058a7624d37e8e8e
Firouz Karimov <fkarimov@gmail.com>
2020-02-11 16:55:40 -0700

    another draft

--- EmployeeSQL/QuickDBD-Free Diagram.sql
+++ EmployeeSQL/QuickDBD-Free Diagram.sql
@@ -11,23 +11,23 @@ CREATE TABLE "salaries" (
 );
 
 CREATE TABLE "titles" (
-    "emp_id" int   NOT NULL,
-    "title" string   NOT NULL,
+    "emp_no" int   NOT NULL,
+    "title" VARCHAR   NOT NULL,
     "from_date" date   NOT NULL,
     "to_date" date   NOT NULL
 );
 
 CREATE TABLE "department" (
-    "dept_no" string   NOT NULL,
-    "dept_name" string   NOT NULL,
+    "dept_no" VARCHAR   NOT NULL,
+    "dept_name" VARCHAR   NOT NULL,
     CONSTRAINT "pk_department" PRIMARY KEY (
         "dept_no"
      )
 );
 
-CREATE TABLE "dept_emplpyee" (
+CREATE TABLE "dept_employee" (
     "emp_no" int   NOT NULL,
-    "dept_no" string   NOT NULL,
+    "dept_no" VARCHAR   NOT NULL,
     "from_date" date   NOT NULL,
     "to_date" date   NOT NULL
 );
@@ -35,9 +35,9 @@ CREATE TABLE "dept_emplpyee" (
 CREATE TABLE "employee" (
     "emp_no" int   NOT NULL,
     "birth_date" date   NOT NULL,
-    "first_name" string   NOT NULL,
-    "last_name" string   NOT NULL,
-    "gender" string   NOT NULL,
+    "first_name" VARCHAR   NOT NULL,
+    "last_name" VARCHAR   NOT NULL,
+    "gender" VARCHAR   NOT NULL,
     "hire_date" date   NOT NULL,
     CONSTRAINT "pk_employee" PRIMARY KEY (
         "emp_no"
@@ -45,7 +45,7 @@ CREATE TABLE "employee" (
 );
 
 CREATE TABLE "dept_manager" (
-    "dept_no" string   NOT NULL,
+    "dept_no" VARCHAR   NOT NULL,
     "emp_no" int   NOT NULL,
     "from_date" date   NOT NULL,
     "to_date" date   NOT NULL
@@ -54,14 +54,17 @@ CREATE TABLE "dept_manager" (
 ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employee" ("emp_no");
 
-ALTER TABLE "titles" ADD CONSTRAINT "fk_titles_emp_id" FOREIGN KEY("emp_id")
+ALTER TABLE "titles" ADD CONSTRAINT "fk_titles_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employee" ("emp_no");
 
-ALTER TABLE "dept_emplpyee" ADD CONSTRAINT "fk_dept_emplpyee_emp_no" FOREIGN KEY("emp_no")
+ALTER TABLE "dept_employee" ADD CONSTRAINT "fk_dept_employee_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employee" ("emp_no");
 
-ALTER TABLE "dept_emplpyee" ADD CONSTRAINT "fk_dept_emplpyee_dept_no" FOREIGN KEY("dept_no")
+ALTER TABLE "dept_employee" ADD CONSTRAINT "fk_dept_employee_dept_no" FOREIGN KEY("dept_no")
 REFERENCES "department" ("dept_no");
+
+ALTER TABLE "employee" ADD CONSTRAINT "fk_employee_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "dept_manager" ("emp_no");
 
 ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
 REFERENCES "department" ("dept_no");

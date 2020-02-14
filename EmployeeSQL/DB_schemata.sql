dadff43af59aaac94c515c837cbe20d523e1c19e
Firouz Karimov <fkarimov@gmail.com>
2020-02-14 12:12:49 -0700

    Home work

---
+++ EmployeeSQL/DB_schemata.sql
@@ -0,0 +1,62 @@
+CREATE TABLE "salaries" (
+    "emp_no" int   NOT NULL,
+    "salary" int   NOT NULL,
+    "from_date" date   NOT NULL,
+    "to_date" date   NOT NULL
+);
+
+CREATE TABLE "titles" (
+    "emp_id" int   NOT NULL,
+    "title" VARCHAR   NOT NULL,
+    "from_date" date   NOT NULL,
+    "to_date" date   NOT NULL
+);
+
+CREATE TABLE "department" (
+    "dept_no" VARCHAR   NOT NULL,
+    "dept_name" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_department" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "dept_employee" (
+    "emp_no" int   NOT NULL,
+    "dept_no" VARCHAR   NOT NULL,
+    "from_date" date   NOT NULL,
+    "to_date" date   NOT NULL
+);
+
+CREATE TABLE "employee" (
+    "emp_no" int   NOT NULL,
+    "birth_date" date   NOT NULL,
+    "first_name" VARCHAR   NOT NULL,
+    "last_name" VARCHAR   NOT NULL,
+    "gender" VARCHAR   NOT NULL,
+    "hire_date" date   NOT NULL,
+    CONSTRAINT "pk_employee" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "dept_manager" (
+    "dept_no" VARCHAR   NOT NULL,
+    "emp_no" int   NOT NULL,
+    "from_date" date   NOT NULL,
+    "to_date" date   NOT NULL
+);
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employee" ("emp_no");
+
+ALTER TABLE "titles" ADD CONSTRAINT "fk_titles_emp_id" FOREIGN KEY("emp_id")
+REFERENCES "employee" ("emp_no");
+
+ALTER TABLE "dept_employee" ADD CONSTRAINT "fk_dept_employee_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employee" ("emp_no");
+
+ALTER TABLE "dept_employee" ADD CONSTRAINT "fk_dept_employee_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "department" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "department" ("dept_no");
